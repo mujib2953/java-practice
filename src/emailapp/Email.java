5a0f4ca773c764cf25a0fd0b323d444425c76dc2
rehmanm9c <mujibur.rehman@citiustech.com>
2019-07-18 16:51:10 +0530

    Added Email propeties and todo list

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,21 @@
 package emailapp;
 
 class Email {
+    private String firtsName;
+    private String lastName;
+    private String password;
+    private String department;
+    private String alternateString;
 
+    private int mailboxCapacity;
+
+    // --- constructor will receive first & last name
+
+    // --- We will ask for department
+
+    // --- generate radnom password
+
+    // --- set mailbox capacity
+    
+    // --- set alternate email / change the password
 }
