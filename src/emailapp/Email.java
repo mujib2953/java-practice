1f8324363b4c74f688140bf41289dbdab03a42b1
rehmanm9c <mujibur.rehman@citiustech.com>
2019-07-18 18:46:05 +0530

    added getDepartment method

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,6 +1,8 @@
 package emailapp;
 
-class Email {
+import java.util.Scanner;
+
+public class Email {
     private String firtsName;
     private String lastName;
     private String password;
@@ -14,6 +16,22 @@ class Email {
         this.lastName = lastName;
 
         System.out.println("EMAIL CREATED :: " + this.firtsName + " " + this.lastName);
+
+        this.department = setDepartment();
+        System.out.println("Selected Department : " + this.department);
+    }
+
+    private String setDepartment() {
+        System.out.println("DEPARTMENT CODES: \n1. Sales \n2.Development \n3. Accounts \n0. None \nEnter the department: ");
+
+        Scanner sc = new Scanner(System.in);
+        int departmentChoice = sc.nextInt();
+        sc.close();
+
+        if (departmentChoice == 1) { return "sales"; }
+        else if (departmentChoice == 2) { return "dev"; }
+        else if (departmentChoice == 3) { return "accounting"; }
+        else { return ""; }
     }
 
     // --- We will ask for department
