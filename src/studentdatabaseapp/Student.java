f37210a746e79919ee9545aced3dcb3343c3f9f7
rehmanm9c <mujibur.rehman@citiustech.com>
2019-07-19 17:25:58 +0530

    student id generated.

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,5 +1,42 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String courses;
+    private String studentID;
     
+    private int gradeYear;
+    private int tutionBalance;
+
+    private static int costOfCourse = 600;
+    private static int id = 1000;
+
+    public Student() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nEnter Studen first Name: ");
+        firstName = sc.nextLine();
+
+        System.out.print("\nEnter Studen last Name: ");
+        lastName = sc.nextLine();
+
+        System.out.print("\n1 - Freshmen. \n2 - Sophmore \n3 - Junior \n4 - Senior");
+        System.out.print("\nEnter student class level : ");
+        gradeYear = sc.nextInt();
+        sc.close();
+
+        setStudentId();
+
+        System.out.println("\n" + firstName + ", " + lastName + " from " + gradeYear + "SID : " + studentID);
+    }
+
+    private void setStudentId() {
+        
+        id++;
+        studentID = gradeYear + "" + id;
+        
+    }
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -2,6 +2,7 @@ package studentdatabaseapp;
 
 public class StudentDatabaseApp {
     public static void main(String... args) {
-        
+
+        Student st1 = new Student();
     }
 }
