0c722aa742765f932736ae71a9471ace6460ff32
rehmanm9c <mujibur.rehman@citiustech.com>
2019-07-22 15:09:05 +0530

    Added enroll method

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -9,13 +9,14 @@ public class Student {
     private String studentID;
     
     private int gradeYear;
-    private int tutionBalance;
+    private int tutionBalance = 0;
 
     private static int costOfCourse = 600;
     private static int id = 1000;
 
+    private static Scanner sc = new Scanner(System.in);
+
     public Student() {
-        Scanner sc = new Scanner(System.in);
 
         System.out.print("\nEnter Studen first Name: ");
         firstName = sc.nextLine();
@@ -23,14 +24,31 @@ public class Student {
         System.out.print("\nEnter Studen last Name: ");
         lastName = sc.nextLine();
 
-        System.out.print("\n1 - Freshmen. \n2 - Sophmore \n3 - Junior \n4 - Senior");
+        System.out.print("\n1 - Freshmen \n2 - Sophmore \n3 - Junior \n4 - Senior");
         System.out.print("\nEnter student class level : ");
         gradeYear = sc.nextInt();
-        sc.close();
-
+        
         setStudentId();
 
-        System.out.println("\n" + firstName + ", " + lastName + " from " + gradeYear + "SID : " + studentID);
+        System.out.println("\n" + firstName + ", " + lastName + " from " + gradeYear + " SID : " + studentID);
+    }
+
+    public void enroll() {
+
+        do {
+            System.out.print("Enter course to enrol (Q to quit): ");
+
+            String course = sc.nextLine();
+
+            if (!course.equals("Q") && sc.hasNext()) {
+                courses = courses + "\n" + course;
+                tutionBalance = tutionBalance + costOfCourse;
+            } else {
+                break;
+            }
+        } while( 1 != 0);
+        System.out.println("ENROLLED IN : " + courses);
+        System.out.println("TUTION BALANCE : " + tutionBalance);
     }
 
     private void setStudentId() {
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -4,5 +4,6 @@ public class StudentDatabaseApp {
     public static void main(String... args) {
 
         Student st1 = new Student();
+        st1.enroll();
     }
 }
